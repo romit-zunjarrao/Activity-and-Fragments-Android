a10f0a7c74ad6a87030ff798faee5b887ab2fdc4
Romit <romitzunjarrao71@gmail.com>
2019-04-10 16:50:14 -0400

    Activity Created

---
+++ app/src/main/java/com/example/activityandfragments/FragmentManager.java
@@ -0,0 +1,23 @@
+package com.example.activityandfragments;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class FragmentManager extends AppCompatActivity {
+
+    ArrayList<AnimalModalClass> arrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_manager);
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+        arrayList = intent.getParcelableExtra(MainActivity.intentIdgit aentifier);
+        Log.d("arrayList",arrayList.toString());
+
+    }
+}
